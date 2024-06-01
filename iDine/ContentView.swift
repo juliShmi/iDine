f8525db2303a039decf93557571c9a14c7b37f47
iuliia.shmitko <iuliia.shmitko@telekom.com>
2024-06-01 22:32:35 +0300

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Iuliia Shmitko on 01/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Iuliia Shmitko on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
