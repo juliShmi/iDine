cad02148a76c84bb9a946c6b86ac82689c004eb1
iuliia.shmitko <iuliia.shmitko@telekom.com>
2024-06-02 14:33:08 +0300

    add ItemRow view

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Iuliia Shmitko on 02/06/2024.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
