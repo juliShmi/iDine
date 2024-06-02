895244b3ced5cf2ac947b4ec68b52bfb7aca74b2
iuliia.shmitko <iuliia.shmitko@telekom.com>
2024-06-02 15:59:31 +0300

    add restrictions and styling for it

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ItemRow: View {
     let menuItem: MenuItem
+    
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+    
     var body: some View {
         HStack {
             Image(menuItem.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.yellow, lineWidth: 2))
             VStack(alignment:.leading) {
                 Text(menuItem.name)
+                    .font(.headline)
                 Text("\(menuItem.price)$")
+            }
+            Spacer()
+            ForEach(menuItem.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
             }
         }
     }
