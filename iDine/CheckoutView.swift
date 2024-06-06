8b9ec034783df7c8e5bb08df3489f4bb7abcf488
iuliia.shmitko <iuliia.shmitko@telekom.com>
2024-06-06 19:53:06 +0300

    add payment  and tip sections to Checkout view

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -12,13 +12,35 @@ struct CheckoutView: View {
     let paymentTypes:[String] = ["Cash", "Credit card", "iDine Points"]
     @State var payBy = "Card"
     
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyCard = ""
+    
+    let tips = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
+    
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay", selection: $payBy) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
+                }
+                Toggle("Add loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter loyalty card number", text: $loyaltyCard)
+                }
+            }
+            Section("Add tips?") {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(tips, id: \.self) {
+                        Text("$\($0)")
+                    }
+                }.pickerStyle(.segmented)
+            }
+            Section("Total: $100") {
+                Button("Confirm order") {
+                    
                 }
             }
         }
